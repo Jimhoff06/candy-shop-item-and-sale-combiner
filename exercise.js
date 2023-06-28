bdec5269e93180b4629eacc6af0808c558877746
Jim <94252877+Jimhoff06@users.noreply.github.com>
2023-06-28 17:47:58 +0000

    completed exercise

--- exercise.js
+++ exercise.js
@@ -1,8 +1,32 @@
 // Your code goes here
 
 module.exports = {
-    // Uncomment these as you write them
-    // getItemById,
-    // buildTransactions,
-    // getTransactionsByItemDescription
+  // Uncomment these as you write them
+  getItemById,
+  buildTransactions,
+  getTransactionsByItemDescription
+};
+
+function getItemById(items, id) {
+  return items.find((item) => {
+    return item.id === id;
+  });
+}
+
+function buildTransactions(sales, items) {
+  return sales.map((sale) => {
+    const item = getItemById(items, sale.itemId);
+    return {
+      itemId: sale.itemId,
+      description: item.description,
+      price: item.price,
+      quantity: sale.quantity,
+    };
+  });
+}
+
+function getTransactionsByItemDescription(transactions, description){
+    return transactions.filter((transaction) => {
+        return transaction.description === description;
+    });
 }
